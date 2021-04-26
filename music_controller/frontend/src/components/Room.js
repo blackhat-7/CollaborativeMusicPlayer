df586584f606f912c12c9e24c09ef718bc874605
Shaunak Pal <palshaunak7@gmail.com>
2021-04-26 17:36:54 +0400

    First commit

---
+++ music_controller/frontend/src/components/Room.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import { render } from "react-dom"
+import {Grid, Button, Typography} from "@material-ui/core"
+import {Link} from "react-router-dom"
+import CreateRoomPage from "./CreateRoomPage"
+
+export default class Room extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            votesToSkip: 2,
+            guestCanPause: false,
+            isHost: false,
+            showSettings: false,
+        };
+        this.roomCode = this.props.match.params.roomCode;
+        this.getRoomDetails();
+        this.handleLeaveRoomPressed = this.handleLeaveRoomPressed.bind(this);
+        this.updateShowSettings = this.updateShowSettings.bind(this);
+    }
+
+    updateShowSettings(val) {
+        this.setState({
+            showSettings: val,
+        });
+    }
+    
+    renderSettingsButton() {
+        return (
+            <Grid item xs={12} align="center">
+                <Button variant='contained' color='primary' onClick={() => this.updateShowSettings(true)}>Settings</Button>
+            </Grid>
+        );
+    }
+
+    renderSettings() {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <CreateRoomPage update={true} 
+                        votesToSkip={this.state.votesToSkip} 
+                        guestCanPause={this.state.guestCanPause} 
+                        updateCallback={}
+                        />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    
+                </Grid>
+            </Grid>
+        );
+    }
+
+    getRoomDetails() {
+        fetch('/api/get-room?code='+this.roomCode)
+            .then((response)=>response.json())
+            .then((data)=>this.setState({
+                votesToSkip:data.votes_to_skip,
+                guestCanPause:data.guest_can_pause,
+                isHost:data.is_host,
+            }));
+    }
+
+    handleLeaveRoomPressed() {
+        const requestOptions = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+        }
+        fetch('/api/leave-room', requestOptions)
+            .then((response) => {
+                this.props.history.push('/');
+            });
+    }
+
+    render() {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Code: {this.roomCode}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h6" component="h6">
+                        Votes: {this.state.votesToSkip}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h6" component="h6">
+                        Pause: {this.state.guestCanPause.toString()}
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h6" component="h6">
+                        isHost: {this.state.isHost.toString()}
+                    </Typography>
+                </Grid>
+                {this.state.isHost ? this.renderSettingsButton() : null}
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" onClick={this.handleLeaveRoomPressed}>Leave Room</Button>
+                </Grid>
+            </Grid>
+        );
+    }
+}
---
+++ music_controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,77 @@
+import React, { Component } from "react";
+import { render } from "react-dom"
+import { Button, TextField, Typography, Grid } from "@material-ui/core"
+import { Link } from "react-router-dom"
+
+export default class RoomJoinPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state={
+            roomCode:"",
+            error:"",
+        };
+        this.handleEnterRoom = this.handleEnterRoom.bind(this);
+        this.handleTextFieldChange = this.handleTextFieldChange.bind(this);
+    }
+
+    handleTextFieldChange(e) {
+        this.setState({
+            roomCode:e.target.value
+        });
+        console.log("handled text field")
+    }
+
+    handleEnterRoom() {
+        console.log(this.state.roomCode);
+        const requestOptions = {
+            method:"Post",
+            headers:{ "Content-Type": "application/json" },
+            body: JSON.stringify({
+                code: this.state.roomCode,
+            })
+        }
+        fetch('/api/join-room', requestOptions)
+            .then((response) => {
+                if (response.ok) {
+                    this.props.history.push(`/room/${this.state.roomCode}`);
+                }
+                else {
+                    this.setState({
+                        error: 'Room not found',
+                    })
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        return (
+            <Grid container spacing={1}>
+                <Grid item xs={12} align="center">
+                    <Typography variant="h4" component="h4">
+                        Join a Room
+                    </Typography>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <TextField
+                        error={this.state.error}
+                        label="Code"
+                        placeholder="Enter a Room Code"
+                        value={this.state.roomCode}
+                        helperText={this.state.error}
+                        variant="outlined"
+                        onChange={this.handleTextFieldChange}
+                    />
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="primary" onClick={this.handleEnterRoom}>Enter Room</Button>
+                </Grid>
+                <Grid item xs={12} align="center">
+                    <Button variant="contained" color="secondary" to="/" component={Link}>Back</Button>
+                </Grid>
+            </Grid>
+        );
+    }
+}
